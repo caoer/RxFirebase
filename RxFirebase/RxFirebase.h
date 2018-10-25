790421349e43b1653aed56cc3d18b002813e2aa1
Zitao Xiong <caoer115@gmail.com>
2018-10-26 03:21:35 +0800

    carthage support

---
+++ RxFirebase/RxFirebase.h
@@ -0,0 +1,19 @@
+//
+//  RxFirebase.h
+//  RxFirebase
+//
+//  Created by Zitao Xiong on 10/26/18.
+//  Copyright © 2018 Zitao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RxFirebase.
+FOUNDATION_EXPORT double RxFirebaseVersionNumber;
+
+//! Project version string for RxFirebase.
+FOUNDATION_EXPORT const unsigned char RxFirebaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxFirebase/PublicHeader.h>
+
+
